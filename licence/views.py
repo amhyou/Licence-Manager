3319c8826337a24636f948862e8dd5bf9565783a
amhyou <younes.amhaouch@hotmail.com>
2024-09-19 19:42:08 +0100

    check_licence with devices endpoint

--- licence/views.py
+++ licence/views.py
@@ -12,13 +12,65 @@ def check_licence(request: HttpRequest):
             'status': 'ko'
         }
         return JsonResponse(data)
-    body = json.loads(request.body)   # { "licence": "qsfdsdfs",  }
+    body = json.loads(request.body)
     print(body)
-    data = {
-        'msg': '123456',
-        'status': 'ok'
-    }
-    return JsonResponse(data)
+    # check if mac and licence are included in the request body
+    key = body.get("licence")
+    mac = body.get("mac")
+    if key is None or mac is None:
+        data = data = {
+            'msg': 'body is not correct',
+            'status': 'ko'
+        }
+        return JsonResponse(data)
+
+    # check if the licence exists
+    licence: Licence = Licence.objects.filter(key=key).first()
+    if licence is None:
+        data = data = {
+            'msg': 'not existing licence',
+            'status': 'ko'
+        }
+        return JsonResponse(data)
+
+    # check if the licence is not expired yet
+    if licence.expiration_date < date.today():
+        data = data = {
+            'msg': 'licence expired',
+            'status': 'ko'
+        }
+        return JsonResponse(data)
+    
+    # check if the licence is still active
+    if licence.status != "active":
+        data = data = {
+            'msg': 'licence is not active',
+            'status': 'ko'
+        }
+        return JsonResponse(data)
+
+    # check if the licence has already maximum devices
+    mac_addresses = Device.objects.filter(licence=licence).values_list('mac', flat=True)
+    print(mac_addresses)
+    if mac in mac_addresses:    
+        data = {
+            'msg': 'mac address valid',
+            'status': 'ok'
+        }
+        return JsonResponse(data)
+    elif len(mac_addresses) < 3:
+        Device.objects.create(licence=licence, mac=mac)
+        data = {
+            'msg': 'mac address added',
+            'status': 'ok'
+        }
+        return JsonResponse(data)
+    else:
+        data = {
+            'msg': 'licence has full devices',
+            'status': 'ko'
+        }
+        return JsonResponse(data)
 
 
 @csrf_exempt
@@ -30,10 +82,10 @@ def add_licence(request: HttpRequest):
         }
         return JsonResponse(data)
 
-    licence = Licence.objects.create(key=str(uuid.uuid4()),expiration_date=date.today()+timedelta(days=30))
+    licence = Licence.objects.create(key=str(uuid.uuid4()), expiration_date=date.today()+timedelta(days=30))
 
     data = {
-        'licence': licence.key,
+        'licence_key': licence.key,
         'days': (licence.expiration_date - licence.issued_date).days,
     }
 
